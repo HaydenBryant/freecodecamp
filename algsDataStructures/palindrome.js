6b110bda8ebc417bc0bc230b11a2ab39d53e3088
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-05-29 14:31:21 -0400

    mults 3 and 5

---
+++ algsDataStructures/palindrome.js
@@ -0,0 +1,14 @@
+// Return true if the given string is a palindrome. Otherwise, return false.
+
+function palindrome(str) {
+    // Good luck!
+    return str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+    var whtSpce = /\s+|_+/g;
+
+    return true;
+  }
+  
+  
+  
+  palindrome("eye");
