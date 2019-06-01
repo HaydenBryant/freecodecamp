4807d78ab8e79629a0121f70a9d6f0d07f6962d7
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-06-01 10:33:23 -0400

    sum odd fibs

--- intJavaAlgs/sumAllFibs.js
+++ intJavaAlgs/sumAllFibs.js
@@ -1,19 +1,39 @@
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+// The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers
+// The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+// For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+
 function sumFibs(num) {
-    var fibNum1 = 1;
-    var fibNum2 = 2;
-    var fibSum = 0;
+  if (num < 0){
+     return -1;
+  }
 
-    for (var i = 1; i <= n; i++) {
-        fibSum = fibNum1 + fibNum2;
-        fibNum1 = fibNum2;
-        fibNum2 = fibSum;
+  if (num === 0 || num === 1){
+    return 1;
+  }
 
-    }
+  var oddFibNum = [1, 1];
+  var fibNum1 = 1;
+  var fibNum2 = 1;
+  var fibSum = 0;
 
-    return num;
+
+  for (var i = 0; fibNum1 <= num; i++){
+      fibSum = fibNum1 + fibNum2;
+      fibNum1 = fibNum2;
+      fibNum2 = fibSum;
+      
+      if (fibSum % 2 !== 0){
+        oddFibNum.push(fibSum);
+      }      
+  }
+
+  return oddFibNum.reduce((acc, curr) => {
+    return acc + curr;
+  });
 }
 
-sumFibs(4);
+sumFibs(10);
 
 
   //for
