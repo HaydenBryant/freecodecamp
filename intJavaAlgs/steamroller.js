ace22bacfa1a38c71c5fb3b0cc04f414bc6c1bc6
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-07-09 13:37:49 -0400

    steamroller

--- intJavaAlgs/steamroller.js
+++ intJavaAlgs/steamroller.js
@@ -1,6 +1,22 @@
+// Flatten a nested array. You must account for varying levels of nesting.
+
+
 function steamrollArray(arr) {
+    // I'm a steamroller, baby
+    var string = arr.toString();
+    var newArr = string.split(",")
+    for (let i = 0; i < newArr.length; i++) {
+      if(Number.isInteger(parseInt(newArr[i]))){
+        newArr[i] = parseInt(newArr[i])
+      }else if(newArr[i] == '[object Object]'){
+        newArr[i] = {};
+      }else if (newArr[i] == '') {
+        newArr.splice(i,1)
+        i--;
+      }
 
-}
+    }
+    return newArr;
 
-// test here
-steamrollArray([1, [2], [3, [[4]]]]);
+  }
+  steamrollArray([1, [], [3, [[4]]]])
