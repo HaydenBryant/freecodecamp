af384844bfa67479877d1bbeec480f26163f64ec
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-09-23 11:55:38 -0400

    refactoring intJavaAlgs

--- intJavaAlgs/difTwoArrays.js
+++ intJavaAlgs/difTwoArrays.js
@@ -1,18 +1,46 @@
-function diffArray(arr1, arr2) {
-  var newArr = [];
-  for (let i = 0; i < arr1.length; i++) {
-        if (arr2.includes(arr1[i])){
-        }else{
-            newArr.push(arr1[i])
-        }
-    }
-    for (let i = 0; i < arr2.length; i++) {
-        if (arr1.includes(arr2[i])){
-        }else{
-            newArr.push(arr2[i])
-        }
-    }
-    return newArr;
-  }
+// function diffArray(arr1, arr2) {
+//   var newArr = [];
+//   for (let i = 0; i < arr1.length; i++) {
+//         if (arr2.includes(arr1[i])){
+//         }else{
+//             newArr.push(arr1[i])
+//         }
+//     }
+//     for (let i = 0; i < arr2.length; i++) {
+//         if (arr1.includes(arr2[i])){
+//         }else{
+//             newArr.push(arr2[i])
+//         }
+//     }
+//     return newArr;
+//   }
   
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+// function diffArray(arr1, arr2) {
+//     let newArr = [];
+//     arr1.forEach (element => {
+//         if(!arr2.includes(element)) newArr.push(element)
+//     });
+
+//     arr2.forEach (element => {
+//         if(!arr1.includes(element)) newArr.push(element)
+//     });
+//     // Same, same; but different.
+//     return newArr;
+//   }
+
+  function diffArray(arr1, arr2) {
+    var newArr = [];
+
+    arr1.filter(element => !arr2.includes(element)); newArr.push(element)
+    arr2.filter(element => !arr1.includes(element)); newArr.push(element)
+
+    return newArr;
+
+  }
+
+//   const diffArray = (arr1, arr2) => checkArrays(arr1, arr2).concat(checkArrays(arr2, arr1))
+//   const checkArrays = (arr1, arr2) => arr1.filter(element => !arr2.includes(element))
+  
+//   diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
--- intJavaAlgs/seekDestroy.js
+++ intJavaAlgs/seekDestroy.js
@@ -1,8 +1,10 @@
-function destroyer(arr) {
-  var args = Array.from(arguments).slice(1);
-  return arr.filter(function (val) {
-    return !args.includes(val);
-  });
-}
+// function destroyer(arr) {
+//   var args = Array.from(arguments).slice(1);
+//   return arr.filter(function (val) {
+//     return !args.includes(val);
+//   });
+// }
+
+const destroyer = (arr, ...args) => arr.filter(element => !args.includes(element))
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
