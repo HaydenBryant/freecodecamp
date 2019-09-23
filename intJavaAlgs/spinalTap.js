634c0511107615076330fb1f9f06158c8c526eb2
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-09-23 13:35:37 -0400

    refactoring/redoing intJavaAlgs

--- intJavaAlgs/spinalTap.js
+++ intJavaAlgs/spinalTap.js
@@ -1,12 +1,20 @@
-function spinalCase(str) {
-  // "It's such a fine line between stupid, and clever."
-  // --David St. Hubbins
+// function spinalCase(str) {
+//   // "It's such a fine line between stupid, and clever."
+//   // --David St. Hubbins
 
-  var whtSpce = /\s+|_+/g;
+//   var whtSpce = /\s+|_+/g;
 
-  str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+//   str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
 
-  return str.replace(whtSpce, '-').toLowerCase();
+//   return str.replace(whtSpce, '-').toLowerCase();
+// }
+
+// spinalCase('This Is Spinal Tap');
+
+
+
+const spinalCase = (str) => {
+  return str.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/\s+|_+/g, '-').toLowerCase();
 }
 
-spinalCase('This Is Spinal Tap');
+spinalCase('thisIsSpinalTap');
--- intJavaAlgs/whereArtThou.js
+++ intJavaAlgs/whereArtThou.js
@@ -1,18 +1,29 @@
-function whatIsInAName(collection, source) {
-    // What's in a name?
-    var arr = [];
-    // Only change code below this line
+// function whatIsInAName(collection, source) {
+//     // What's in a name?
+//     var arr = [];
+//     // Only change code below this line
 
-    var keys = Object.keys(source);
+//     var keys = Object.keys(source);
 
-    arr = collection.filter(element => {
-        return keys.every(key => {
-            return element[key] && element[key] === source[key]
-        })
-    })
+//     arr = collection.filter(element => {
+//         return keys.every(key => {
+//             return element[key] && element[key] === source[key]
+//         })
+//     })
 
-    // Only change code above this line
-    return arr;
+//     // Only change code above this line
+//     return arr;
+// }
+
+const whatIsInAName = (collection, source) => { return (collection.filter(element => checkObjectForSourceKeyValuePairs(source, element)))}
+
+const checkObjectForSourceKeyValuePairs = (source, obj) => {
+    let check = true;
+    Object.keys(source).forEach(key => {
+        if(source[key] !== obj[key]) check = false;
+    });
+    return check;
 }
+
 
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
