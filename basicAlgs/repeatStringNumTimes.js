eb1fdf24410a624ba66b60e5659db525fb9ec279
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-09-29 14:08:54 -0400

    repeatStringNumTimes

---
+++ basicAlgs/repeatStringNumTimes.js
@@ -0,0 +1,19 @@
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+// This challenge can be solved with the .endsWith()method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+
+function repeatStringNumTimes(str, num) {
+  let fullStr = "";
+  if(num < 0)
+    return "";
+  if(num === 1)
+    return str;
+  else
+    for(var i = 0; i < num; i++){
+      fullStr += str;
+    }
+    return fullStr;
+}
+  
+  confirmEnding("Bastian", "n");
