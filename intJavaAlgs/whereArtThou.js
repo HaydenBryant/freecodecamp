e3705c6dc108582be5c425332fd4ecbbdad400be
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-09-30 16:31:36 -0400

    in class work

--- intJavaAlgs/whereArtThou.js
+++ intJavaAlgs/whereArtThou.js
@@ -15,15 +15,64 @@
 //     return arr;
 // }
 
-const whatIsInAName = (collection, source) => { return (collection.filter(element => console.log(source, element)))}
+// const whatIsInAName = (collection, source) => { return (collection.filter(obj => checkObjectForSourceKeyValuePairs(source, obj)))}
 
-const checkObjectForSourceKeyValuePairs = (source, obj) => {
-    let check = true;
-    Object.keys(source).forEach(key => {
-        if(source[key] !== obj[key]) check = false;
-    });
-    return check;
+// const checkObjectForSourceKeyValuePairs = (source, obj) => {
+//     let check = true;
+//     Object.keys(source).forEach(key => {
+//         if(source[key] !== obj[key]) check = false;
+//     });
+//     return check;
+// }
+
+// whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+
+
+
+
+
+function whatIsInAName(collection, source) {
+    var keys = Object.keys(source);
+    return (collection.filter(obj => {
+        for(var key = 0; key < keys.length; key++){
+            if(source[keys[key]] !== obj[keys[key]]){
+                return false;
+            }
+        }
+        return true;
+    }));
 }
 
-
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+// function whatIsInAName(collection, source) {
+//     // What's in a name?
+//     var arr = [];
+//     // Only change code below this line
+//     var keys = Object.keys(source);
+//     for (var item = 0; item < collection.length; item++)
+//     {
+//       var isGood = true;
+//       for (var key = 0; key < keys.length; key++)
+//       {
+//         if (!collection[item].hasOwnProperty(keys[key])){
+//           isGood = false;
+//           break;
+//         }
+//         // key exists
+//         if (collection[item][keys[key]] !== source[keys[key]])
+//         {
+//           isGood = false;
+//           break;
+//         }
+//       }
+//       if (isGood)
+//       {
+//         arr.push(collection[item])
+//       }
+//     }
+    
+//     // Only change code above this line
+//     return arr;
+//   }
+  
+  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
