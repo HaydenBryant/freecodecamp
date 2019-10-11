ea1fa9139dce96c09b0930db9a69e4d9f216a8fe
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-10-11 13:00:50 -0400

    fixed sumall

--- intJavaAlgs/sumAll.js
+++ intJavaAlgs/sumAll.js
@@ -1,7 +1,15 @@
 function sumAll(arr) {
   var sum = 0;
   
-  for (var i = Math.min(...arr); i <= Math.max(...arr); i++) sum += i;
+  if(arr[0] > arr[1]){
+    for(var i = arr[1]; i <= arr[0]; i++){
+      sum += i;
+    }
+  } else if(arr[1] > arr[0]){
+    for(var i = arr[0]; i <= arr[1]; i++){
+      sum += i;
+    }
+  }
 
   return (sum);
 }
